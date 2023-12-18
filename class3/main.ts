bfddb33451418d81e816439b70e27db985f4d36e
Muhammad Bin Zohaib <144327624+ProMBZ@users.noreply.github.com>
2023-12-18 10:24:00 +0500

    Add files via upload

---
+++ class3/main.ts
@@ -0,0 +1,81 @@
+// let _username:any
+// _username="Sir Mustafa"
+// _username="456"
+// console.log(_username)
+
+
+// let _username:any
+// _username="Sir Mustafa"
+// _username="456"
+// console.log(_username)
+
+
+// console.log("hello")
+
+
+// let studentGender = "Female";
+
+// if (studentGender === "Male") {
+//     console.log("Apki entry left side se hai")
+// }
+
+// else {
+//     console.log("Appki entry right se hai")
+//}
+
+// let salary=4000
+// let monthlyexpense = 8000
+// if (salary===4000) {
+//     console.log("you have not enough money")
+// }else{
+//     console.log("you have enough money")
+// }
+
+
+// console.log("Taking Decision");
+// let raita=true
+// let biscuit=true
+// if(raita){
+//     console.log("Buy Samosa")
+// }
+// else if(biscuit){
+//     console.log("Buy Biscuit")
+// }
+// else{
+//     console.log("Don't buy")
+// }
+
+ // single = assign
+ let num:any = "2"
+ console.log(num) // 2
+ // double == match
+ console.log(num == 2) // false
+ //triple === match - data type
+ console.log(num === 2) // false
+   
+
+
+// for checking the money that you have is that like i have cycle rs1000 and i have 5000 rs sit will give me eror
+
+console.log("Taking Decision");
+let budget= 50000
+let cycle = 40000
+let color="black"
+if((color=="blue" || color=="black" || color =="red")  && cycle <= budget  ) {
+    console.log("Buy Cycle")
+ } 
+else {
+    console.log("I need to work hard and earn money")
+}
+
+
+
+
+
+
+
+
+
+
+
+
