5819f56450eb7dce79c126c7235da0c4b90e8b53
Muhammad Bin Zohaib <144327624+ProMBZ@users.noreply.github.com>
2023-12-18 10:26:00 +0500

    Add files via upload

---
+++ class4/main.ts
@@ -0,0 +1,70 @@
+
+
+// table of 2
+
+// for (let a = 1;a <= 10;a++){
+//     let ans = a * 2
+//     console.log("2", "x", a, "=", ans)
+// }
+
+//abc all words
+
+// let arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+// for(let index = 0;index <=10; index++ ){
+
+//     console.log(arr[index])
+// }
+
+
+
+//loops
+
+// const std = ["Hamzah", "Arham", "Osama"]
+
+// for ( let i = 0; i < 3; i++ ) {
+
+//     console.log(  std [0] );
+// }
+
+//parent loop
+
+// for (let i = 0; i < 3; i++){
+//     console.log("Parent Loop", i)
+
+
+//     for (let j = 0; j < 4; j++){
+//         console.log("Child Loop", j)
+//     }
+// }
+
+// let videos = ["video1", "video2", "video3", "video4"];
+
+// // console.log(videos.lenght) 5
+
+// for (let i = 0; i< videos.length; i++){
+//    console.log(videos[i])
+// }
+
+// let video1Title= "video1"
+// let video1Description = "video 1 description"
+// let video1Image = "https://image.com"
+
+// try this
+
+let video1 = {
+    title: "video1",
+    description: "video 1 description" ,
+    image: "https://image.com"
+}
+
+const videos = [
+    (
+        title: "video2",
+        description: "video 2 description" ,
+        image: "https://image.com"
+    )
+]
+
+console.log(videos[1].)
+
