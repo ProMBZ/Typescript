e5a2244ba2f9a6a4979612757e27df6a1496d308
Muhammad Bin Zohaib <144327624+ProMBZ@users.noreply.github.com>
2023-12-18 10:22:55 +0500

    Add files via upload

--- class2/main.js
+++ class2/main.js
@@ -1,1 +1,30 @@
-
+"use strict";
+let powder = "Lal mirch powder";
+powder = "Madni Pakwan Center";
+// tsc main.ts && node main.js
+//or you can use this node main.ts for seeing your file name
+console.log(powder);
+let fullName = "Muhammad Bin Zohaib";
+//let bulb = true;
+//let bulb = false
+let firstName = "Muhammad";
+let lastNmae = "Bin Zohaib";
+//console.log(firstName + " " + lastName)
+// let qty = 8;
+// console.log(qty + 10);
+//console.log(qty - 1);
+//console.log(qty * 12000);
+//console.log(qty / 12000)
+//let n1 = 10;
+//let n2 = 3;
+//console.log(n1 % n2); 
+//  let age = 22;
+// // age +=10
+// age -=10;
+//  console.log(age)
+let age = 22;
+//double == foe checking number and for checking data ===
+console.log(22 === 22);
+// console.log(age != 22);
+// console.log(age > 10);
+// console.log(age >= 10)
