87f48b173afe762c0cba5b008efbcc4949b89024
Muhammad Bin Zohaib <144327624+ProMBZ@users.noreply.github.com>
2024-01-15 18:59:03 +0500

    Add files via upload

--- countDownTimer/index.ts
+++ countDownTimer/index.ts
@@ -1,1 +1,37 @@
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
 
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please Enter The Amount, Of Second...",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "Please Enter Valid Number?";
+        } else if (input > 60) {
+            return "Seconds Must Be In 60";
+        } else {
+            return true;
+        }
+    },
+});
+
+let input = res.userInput;
+
+function startTime(val: number) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Timer Has Expired...");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }, 1000);
+}
+
+startTime(input);
