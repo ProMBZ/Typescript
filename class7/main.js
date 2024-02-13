99cee8c4d1e4255014378eee7efb9bed603bb96a
Muhammad Bin Zohaib <144327624+ProMBZ@users.noreply.github.com>
2024-02-13 20:25:22 +0500

    Add files via upload

---
+++ class7/main.js
@@ -0,0 +1,72 @@
+// export const a = "Hamzah";
+// let a: string | number | boolean = "Muhammad";
+// Union
+// a = 10
+// a = "Muhammad"
+// a = true
+// Number
+// let age: number = 22;
+// console.log(age.toFixed(2))
+// UpperCase LowerCase
+// let firstName: string = "Muhammad";
+// firstName.toLowerCase()
+// Narrowing
+// let age: string | number;
+// age = 22
+// age.toFixed(2); //Narrowing
+// age = "22";
+// string
+// let age = "22";
+// console.log(typeof age)
+//  let age:string|number = "22";
+// Wrong Method
+// if(typeof age ==="string") {
+//     age.toLocaleUpperCase()
+// }  else {
+//     age.
+// }
+// console.log(Math.random)
+// Math Random Type Plant
+// let newAge = Math.random() > 0.5 ? 22 : "Khan"
+// if (typeof newAge === "string"){
+//     newAge.toUpperCase
+// }  else {
+//      newAge.toFixed(2)
+// }
+// Union Literals
+// let trafficLights:"yellow" | "red" | 3 = 3;
+// trafficLights = 3;
+// clases   // Practice this quiz mae ai ga
+// let teacher: {
+//     name: string,
+//     salary: number
+//     "full-name": string
+// } = {
+//     name: "Muhammad",
+//     salary: 40000,
+//     "full-name" : "Muhammad Bin Zohaib"
+// }
+// teacher.name //Muhammad
+// console.log(teacher["full-name"])
+let teacher = {
+    name: "Zeeshan",
+    exp: 10
+};
+export {};
+// Same Here But Also See This to ra sa change ha
+//  type Student =  string | boolean | number | undefined
+// let b: string | boolean | number |undefined
+//  let a: Student;
+//  let b: Student;
+// type Student = {
+//     name: string,
+//     age: number
+// }
+// let student1: Student = {
+//     name: "Muhammad",
+//     age: 11
+// }
+// let student2: Student = {
+//     name: "Abu Bakar",
+//     age: 11
+// }
