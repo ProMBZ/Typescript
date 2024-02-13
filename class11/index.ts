523a583f74beefcdd7d623d4c8b7202ac57ff9c0
Muhammad Bin Zohaib <144327624+ProMBZ@users.noreply.github.com>
2024-02-13 20:31:21 +0500

    Add files via upload

--- class11/index.ts
+++ class11/index.ts
@@ -1,1 +1,68 @@
+//                       OBJECT ORIENTED PROGRAMMING  (OOP)
+
+
+// When You Will Write The Name Of Class It Will Be Capital...
+
+// class House {
+//     // numberOfRooms: number = 4;    
+//     // numberOfBathrooms: number = 1; 
+//     constructor(nOfRooms: number, nOfBathrooms: number) {
+//     //   console.log(nOfRooms, nOfBathrooms);
+//     // this.numberOfBathrooms = nOfBathrooms
+//     // this.numberOfRooms = nOfRooms
+//     };
+// };
+
+// // {numberOfBathrooms:4, numberOfRooms:2}
+
+// let house1 = new House(4, 2);
+// let house2 = new House(3, 1);
+// house1.numberOfBathrooms = 1
+// console.log(house1);
+// console.log(house2);
+
+//  console.log(house1.numberOfRooms);
+
+// Try This Person Class And Details...
+
+// class Person {
+//    name: string = "Muhammad Bin Zohaib"
+//    age: number = 11
+//    rollNumber: number = 3456
+//    fatherName: string = "Syed Zohaib Tanwir"
+//    car: string = "Honda (Civic)"
+//    carModel: number = 2021
+//    houseNumber: string = "AA11"
+//      school: string "The Inspiration Model School"
+//      course: string "Web3.0 Ai and Meatverse Blockchain"
+// }
+
+// let person1 = new Person();
+
+// console.log(person1)
+
+// -------------------------------------------------------------------------------------------------
+
+
+class Person1 {
+    name:string
+    salary!:number
+    constructor(name: string, salary: number) {
+        this.name = name
+         this.salary = salary
+    };
+};
+
+class Person2{
+    name:string
+    hobby: string
+    constructor(name: string, hobby: string) {
+        this.name = name
+        this.hobby = hobby
+    }
+}
+
+
+
+//                              Inheritance
 
